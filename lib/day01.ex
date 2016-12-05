dbf5f9a267c96fb18f827a170951abe9f172b8ab
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-12-05 20:12:20 +0100

    This is real ugly

--- lib/day01.ex
+++ lib/day01.ex
@@ -22,6 +22,47 @@ defmodule Day01 do
     end
   end
 
+  def find_intersection(directions_string) do
+    do_find_intersection(:up,
+                         0,
+                         0,
+                         MapSet.new,
+                         parse_directions(directions_string))
+  end
+
+  defp do_find_intersection(_, _, _, _, []), do: nil
+
+  defp do_find_intersection(dir, x, y, coords, [step | steps]) do
+    [turn, move] = parse_step(step)
+    new_dir = change_dir(dir, turn)
+    [new_x, new_y] = case new_dir do
+      :up -> [x, y + move]
+      :right -> [x + move, y]
+      :down -> [x, y - move]
+      :left -> [x - move, y]
+    end
+    [_start | new_coords] = traverse_single_step(x, y, new_x, new_y)
+    intersection = Enum.find(new_coords,
+                             fn point -> MapSet.member?(coords, point) end)
+    case intersection do
+      nil ->
+        do_find_intersection(new_dir,
+                            new_x,
+                            new_y,
+                            MapSet.union(coords, MapSet.new(new_coords)),
+                            steps)
+      [x, y] -> abs(x) + abs(y)
+    end
+  end
+
+  defp traverse_single_step(x, y1, x, y2) do
+    Enum.map(y1..y2, fn y -> [x, y] end)
+  end
+
+  defp traverse_single_step(x1, y, x2, y) do
+    Enum.map(x1..x2, fn x -> [x, y] end)
+  end
+
   defp parse_step(step) do
     [turn_code | move_value] = step
     turn = case turn_code do
--- test/day01_test.exs
+++ test/day01_test.exs
@@ -1,19 +1,27 @@
 defmodule Day01Test do
   use ExUnit.Case
 
-  test "empty list" do
+  test "distance for empty list" do
     assert Day01.distance("") == 0
   end
 
-  test "R2 L3" do
+  test "distance for R2 L3" do
     assert Day01.distance("R2, L3") == 5
   end
 
-  test "R2 R2 R2" do
+  test "distance for R2 R2 R2" do
     assert Day01.distance("R2, R2, R2") == 2
   end
 
-  test "R5 L5 R5 R3" do
+  test "distance for R5 L5 R5 R3" do
     assert Day01.distance("R5, L5, R5, R3") == 12
+  end
+
+  test "find_intersection for empty list" do
+    assert Day01.find_intersection("") == nil
+  end
+
+  test "find_intersection for R8 R4 R4 R8" do
+    assert Day01.find_intersection("R8, R4, R4, R8") == 4
   end
 end

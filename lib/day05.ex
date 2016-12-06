0db423256e50ff501fbb1e54492bdfbc641143e2
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-12-06 23:36:02 +0100

    phreak the password

--- lib/day05.ex
+++ lib/day05.ex
@@ -3,6 +3,10 @@ defmodule Day05 do
     do_haxxor(code, 0, [])
   end
 
+  def phreak(code) do
+    do_phreak(code, 0, %{})
+  end
+
   defp do_haxxor(_, _, found) when length(found) == 8 do
     found
       |> Enum.reverse
@@ -16,11 +20,39 @@ defmodule Day05 do
     end
   end
 
+  defp do_phreak(_, _, found) when map_size(found) == 8 do
+    (0..7)
+      |> Enum.map(fn idx -> Map.get(found, idx) end)
+      |> Enum.join
+  end
+
+  defp do_phreak(code, idx, found) do
+    case check_idx_and_pos(code, idx) do
+      { :ok, letter, pos } -> do_phreak(code, idx + 1, Map.put_new(found, pos, letter))
+      :not_found -> do_phreak(code, idx + 1, found)
+    end
+  end
+
   defp check_idx(code, idx) do
     hash = md5(code, idx)
     case String.starts_with?(hash, "00000") do
       true -> { :ok, String.at(hash, 5) }
       false -> :not_found
+    end
+  end
+
+  defp check_idx_and_pos(code, idx) do
+    case parse_hash(md5(code, idx)) do
+      [pos, digit] when pos < 8  -> { :ok, digit, pos }
+      _ -> :not_found
+    end
+  end
+
+  defp parse_hash(hash) do
+    case hash do
+      "00000" <> <<pos::bytes-size(1)>> <> <<digit::bytes-size(1)>> <> _ ->
+        [String.to_integer(pos, 16), digit]
+      _ -> :not_found
     end
   end
 
--- test/day05_test.exs
+++ test/day05_test.exs
@@ -5,4 +5,10 @@ defmodule Day05Test do
   test "finds the password for example case" do
     assert Day05.haxxor("abc") == "18f47a30"
   end
+
+  @tag :skip
+  @tag timeout: 120000
+  test "finds the more 1337 password too" do
+    assert Day05.phreak("abc") == "05ace8e3"
+  end
 end
